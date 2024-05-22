3c5f55c6cf5a05f2f51c154c2c509b085882ef2f
Lucas Simidu <lucas.simidu@outlook.com>
2024-05-22 11:20:48 -0300

    Att Loja.py e Funcoes.py

--- loja.py
+++ loja.py
@@ -1,2 +1,16 @@
+from funcoes import Escolha
+
+listaProdutos = ['NFT1', 'NFT2', 'NFT3']
+opcoes = ['continuar', 'sair']
+
 print('Olá, bem vindo a nossa Loja Virtual!')
-opc = input('Você deseja')
+opc = Escolha(opcoes, 'Você deseja continuar ou voltar para a tela inicial? \n(continuar/sair) ->')
+
+if opc == "sair":
+    #AJUSTAR ISSO PARA VOLTAR PARA O MAIN
+    print('voltar para o main.py')
+else:
+    print('Produtos disponíveis em nossa Loja:')
+    for produto in listaProdutos:
+        i = 0
+        print(f'{i += 1} {produto}')
