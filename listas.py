20e530356a9071f1729532adc6a59591375b75c5
Lucas Simidu <lucas.simidu@outlook.com>
2024-05-23 10:53:19 -0300

    Att listas.py e loja.py

---
+++ listas.py
@@ -0,0 +1,9 @@
+listaProdutos = ['NFT1', 'NFT2', 'NFT3']
+
+iden = ['1', '2', '3']
+
+
+
+produto_dict = dict(zip(iden, listaProdutos))
+
+opcoes = ['continuar', 'sair']
--- loja.py
+++ loja.py
@@ -1,9 +1,7 @@
 from funcoes import Escolha
+from listas import listaProdutos, iden, produto_dict, opcoes
 
-listaProdutos = ['NFT1', 'NFT2', 'NFT3']
-iden = ['1', '2', '3']
 
-opcoes = ['continuar', 'sair']
 
 print('Olá, bem vindo a nossa Loja Virtual!')
 opc = Escolha(opcoes, 'Você deseja continuar ou voltar para a tela inicial? \n(continuar/sair) -> ')
@@ -14,6 +12,12 @@ if opc == "sair":
 else:
     print('----- PRODUTOS -----\n')
     for i, produto in enumerate(listaProdutos, start=1):
-        print(f'{iden[i-1]}. {produto}\n')
-    opcNft = Escolha(iden,'Deseja visualizar algum dos NFTs?\n-> ')
+        print(f'{i}. {produto}\n')
+        i += 1
+    opcNft = Escolha(iden,'Deseja visualizar algum dos NFTs?\nSelecione -> ')
+
+    if opcNft in produto_dict:
+        print(f'{opcNft}. {produto_dict[opcNft]}')
+    else:
+        print('Produto não listado')
 
