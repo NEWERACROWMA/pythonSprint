cbf14d5a97e55e9627f66e76d3d1e7758dda934a
Marcos Costa <79596734+MarcosCosta-dv@users.noreply.github.com>
2024-05-23 10:25:08 -0300

    Att. Funções
    Reorganização das funções da pagina de bet

--- bet.py
+++ bet.py
@@ -1,25 +1,10 @@
+from funcoes import obter_aposta, obter_resposta, Escolha
+
 equipes = ['Mahindra Racing', 'Jaguar TCS Racing', 'Maserati MSG Racing', 'Nissan Formula E Team']
 Mahindra_corredores = ['Edoardo Mortara','Nick De Vries']
 Jaguar_corredores = ['Mitch Evans','Nick Cassidy']
 Maserati_corredores = ['Jehan Daruvala','Maximilian Gunther'] 
 Nissan_corredores = ['Oliver Rowland','Sacha fenestraz']
-
-
-def obter_resposta():
-    while True:
-        resposta = input(f"Em qual equipe você deseja apostar?\n{', '.join(equipes)}\n--> ")
-        if resposta in equipes:
-            return resposta
-        else:
-            print("Essa equipe não existe, escolha uma opção válida")
-
-def obter_aposta():
-    while True:
-        aposta = input("Quanto você deseja apostar?\n--> ")
-        if aposta.isnumeric():
-            return int(aposta)
-        else:
-            print("Você não digitou um valor válido, por favor digite apenas números")
 
 print("Bem vindo à nossa aba de apostas da Formula-E!")
 
--- funcoes.py
+++ funcoes.py
@@ -4,3 +4,18 @@ def Escolha(lista_opcoes, msg):
         escolha = input(msg)
     return escolha
 
+def obter_resposta():
+    while True:
+        resposta = input(f"Em qual equipe você deseja apostar?\n{', '.join(equipes)}\n--> ")
+        if resposta in equipes:
+            return resposta
+        else:
+            print("Essa equipe não existe, escolha uma opção válida")
+
+def obter_aposta():
+    while True:
+        aposta = input("Quanto você deseja apostar?\n--> ")
+        if aposta.isnumeric():
+            return int(aposta)
+        else:
+            print("Você não digitou um valor válido, por favor digite apenas números")
