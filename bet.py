10d8dc6718d32b24162cb44e2c5826e5d9a89b7c
Gabriel Lilla <gabrielterralilla@gmail.com>
2024-06-10 11:00:42 -0300

    Update bet.py

--- bet.py
+++ bet.py
@@ -2,11 +2,9 @@ from funcao_BET import adicionar_aposta, exibir_resumo, perguntar_continuar
 
 print("Bem-vindo à nossa aba de apostas da Formula-E!")
 
-# Inicialização das variáveis
 total_apostas = 0
 apostas = {}
 
-# Loop principal do programa para apostas nas equipes e corredores
 while True:
     total_apostas = adicionar_aposta(apostas, total_apostas)
     
